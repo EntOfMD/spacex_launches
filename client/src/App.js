8fc00778d5e475168da1e99ee135fff122d7cbba
Ashell Fernando <ashellz2010@gmail.com>
2019-03-03 15:37:23 -0500

    added logo and apollo

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={logo} alt="spacex dark logo" style={{width:300, display:'block', margin:'auto'}} />
+        <img
+          src={logo}
+          alt="spacex dark logo"
+          style={{ width: 300, display: "block", margin: "auto" }}
+        />
       </div>
     );
   }
