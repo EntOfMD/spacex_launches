e4eaeeb91f8ed326cadc44837b8d68f97e06f98f
Ashell Fernando <ashellz2010@gmail.com>
2019-03-03 14:22:46 -0500

    server page

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true,
+	})
+);
+
+app.listen(PORT, () => {
+	console.log(`Server started on ${PORT}`);
+});
