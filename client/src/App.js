7830cd52643e373ca339ca2e2c1410294f35ecac
Ashell Fernando <ashellz2010@gmail.com>
2019-03-03 15:41:24 -0500

    apollo linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.jpg";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
+import logo from "./logo.jpg";
 import "./App.css";
+
+const client = new ApolloClient({
+  uri: `http://localhost:5000/graphql`
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img
-          src={logo}
-          alt="spacex dark logo"
-          style={{ width: 300, display: "block", margin: "auto" }}
-        />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            src={logo}
+            alt="spacex dark logo"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+        </div>
+      </ApolloProvider>
     );
   }
 }
